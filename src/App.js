ea2bd558ef9344ea2fee483e57f1213a9a82a299
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2023-12-22 16:16:56 +0900

    Secrets Test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Workflow Test
+          Using Secrets
         </p>
         <a
           className="App-link"
